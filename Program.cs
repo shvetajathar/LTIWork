677121a81ef3358c0c77198651c9f177c1574452
shvetajathar <shveta28jathar@gmail.com>
2023-10-20 11:38:05 +0000

    fisrt commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Entity;
+using BZLayer;
+
+MovieBz bz=new MovieBz();
+List<Movie> movies=bz.GetMovies();
+if(movies!=null)
+{
+    foreach(var m in movies)
+    {
+        Console.WriteLine($"{m.Id} {m.name} {m.Ratings} {m.Ryear}");
+    }
+}
+else{
+    Console.WriteLine("No Movies Present");
+}
